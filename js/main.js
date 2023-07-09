dfe4385436c4535dc94e44d30d5c4267fd829a23
erikrex007Yimmy <bcapybara63@gmail.com>
2023-07-09 13:20:36 +0200

    A lot

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 function copyIP() {
   /* Copy text into clipboard */
   navigator.clipboard.writeText("county-conservation.craft.ply.gg");
+  //alert("IP copied");
+}
+function copyDiscord() {
+  /* Copy text into clipboard */
+  navigator.clipboard.writeText("https://discord.gg/n3BUVxa6hs");
+  //alert("IP copied");
 }
